6c36f820f8f368dbad0db0fa4a2b9bbce29a1050
gpkota2306-dev <gpkota2306@gmail.com>
2026-01-02 20:49:16 +0530

    Add Patient class

---
+++ src/src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    int id;
+    String name;
+    int age;
+    String disease;
+
+    public Patient(int id, String name, int age, String disease) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.disease = disease;
+    }
+
+    public void display() {
+        System.out.println(id + " | " + name + " | " + age + " | " + disease);
+    }
+}
