e42b46cc7b1a4f4f3f2625d58b2634044fac9979
gpkota2306-dev <gpkota2306@gmail.com>
2026-01-02 20:54:25 +0530

    Hospital.java

---
+++ src/src/Doctor.java
@@ -0,0 +1,15 @@
+public class Doctor {
+    int id;
+    String name;
+    String specialization;
+
+    public Doctor(int id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+    public void display() {
+        System.out.println(id + " | " + name + " | " + specialization);
+    }
+}
